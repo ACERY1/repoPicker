da0cf1d2cdb3e640a5d05d9fd977dbdbbd3ee60a
ACERY1 <y841034081@gmail.com>
2017-05-27 13:14:09 +0800

    fix

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const PORT = 1258;
-
-app.use(bodyParser.json());
 let app = express();
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
 	console.log(req.body);
